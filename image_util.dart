ed3d51186124fe015da3f59140ad707cac168a2c
Shellie2003 <113421731+Shellie2003@users.noreply.github.com>
2025-04-04 16:04:46 +0200

    Create image_util.dart
    pour les image

---
+++ image_util.dart
@@ -0,0 +1,12 @@
+// Retrieves a random image by keyword from Unsplash
+// Keyword must be a single word STRING LITERAL (not variable), such as "Peace Lily" "Niagara Falls" "Poodle". Be specific about the keyword and add a generic category.
+// imageType must be one of "photo", "illustration" or "vector"
+// category can be "backgrounds", "fashion", "nature", "science", "education", "feelings", "health", "people", "religion", "places", "animals", "industry", "computer", "food", "sports", "transportation", "travel", "buildings", "business" or "music"
+// colors can be one of the following values: "grayscale", "transparent", "red", "orange", "yellow", "green", "turquoise", "blue", "lilac", "pink", "white", "gray", "black" or "brown"
+String getRandomImageByKeyword(String keyword, {
+  String imageType = "photo",
+  String? category,
+  String? colors,
+}) {
+  return 'implementation will be provided in production; no need to implement.';
+}
